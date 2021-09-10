5461a0ff5f925f4f7a1118a5e6b69ddb077b1848
LordOrbit <49206768+LordOrbit@users.noreply.github.com>
2021-09-10 12:33:00 +0300

    BETA v.0.2.0

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,45 +1,53 @@
-const imageMapSize = 11
-const pixelSize = 46
-const imageSize = pixelSize * imageMapSize
 const colors = [[225, 30, 30], [30, 225, 30], [30, 30, 225],
     [190, 60, 210], [220, 220, 90], [210, 120, 110], [150, 220, 225]]
-
-function rgb2hsv(r, g, b) {
-    let v = Math.max(r, g, b), c = v - Math.min(r, g, b);
-    let h = c && ((v === r) ? (g - b) / c : ((v === g) ? 2 + (b - r) / c : 4 + (r - g) / c));
-    return [60 * (h < 0 ? h + 6 : h), v && c / v, v];
-}
+const defaultSize = 512
 
 function hsv2rgb(h, s, v) {
-    let f = (n, k = (n + h / 60) % 6) => v - v * s * Math.max(Math.min(k, 4 - k, 1), 0);
+    s /= 100
+    v /= 255
+    let f = (n, k = (n + h / 60) % 6) => (v - v * s * Math.max(Math.min(k, 4 - k, 1), 0)) * 255;
     return [f(5), f(3), f(1)];
 }
 
-function generateImage(imageSize, imageMapSize, h = 'set', s = 10, v = 30, color = 'random') {
+function generateImage(pixelCount = 'set', margin = 'set', h = 'set', s = 'set', v = 'set') {
+    if (pixelCount === 'set')
+        pixelCount = +document.querySelector("#count").value
+    if (margin === 'set')
+        margin = +document.querySelector("#margin").value
+    const pixelSize = Math.trunc(Math.round(defaultSize / pixelCount))
+    const imageSize = pixelSize * pixelCount
+
+
     const canvas = document.querySelector('#image')
     const ctx = canvas.getContext("2d")
     canvas.height = imageSize
     canvas.width = imageSize
+    ctx.beginPath();
+    ctx.rect(0, 0, imageSize, imageSize);
+    ctx.fillStyle = "white";
+    ctx.fill();
 
-    if (color === 'random')
-        color = colors[Math.trunc(Math.random() * colors.length)]
-    color = rgb2hsv(color[0], color[1], color[2])
     if (h === 'set')
         h = +document.querySelector("#canal-h").value
-    color[0] = h
-    s /= 100
-    const firstColor = hsv2rgb(color[0], color[1], color[2])
-    const secondColor = hsv2rgb(color[1] >= 180 ? color[0] - h : color[0] + h, color[1] >= .5 ? color[1] - s : color[1] + s, color[2] >= 125 ? color[2] - v : color[2] + v)
+    if (s === 'set')
+        s = +document.querySelector("#canal-s").value
+    if (v === 'set')
+        v = +document.querySelector("#canal-v").value
+    const difH = 40
+    const difS = 10
+    const difV = 30
+    const firstColor = hsv2rgb(h, s, v)
+    const secondColor = hsv2rgb(h >= 180 ? h - difH : h + difH, s >= 50 ? s - difS : s + difS, v >= 125 ? v - difV : v + difV)
 
     let image = []
-    for (let i = 0; i < imageMapSize; i++) {
+    for (let i = 0; i < pixelCount; i++) {
         image.push([])
-        for (let j = 0; j < imageMapSize; j++)
+        for (let j = 0; j < pixelCount; j++)
             image[i].push(0)
     }
-    for (let i = 0; i < imageMapSize; i++)
-        for (let j = 0; j < (imageMapSize + 1) / 2; j++)
-            image[i][j] = image[i][imageMapSize - j - 1] = Math.trunc((Math.random() * 2))
+    for (let i = margin; i < pixelCount - margin; i++)
+        for (let j = margin; j < (pixelCount + 1) / 2; j++)
+            image[i][j] = image[i][pixelCount - j - 1] = Math.trunc((Math.random() * 2))
 
     ctx.lineWidth = 1
     for (let i = 0; i < imageSize; i++) {
@@ -47,7 +55,7 @@ function generateImage(imageSize, imageMapSize, h = 'set', s = 10, v = 30, color
         const lineColor = [firstColor[0] * (1 - offset) + secondColor[0] * offset,
             firstColor[1] * (1 - offset) + secondColor[1] * offset,
             firstColor[2] * (1 - offset) + secondColor[2] * offset]
-        for (let j = 0; j < imageMapSize; j++) {
+        for (let j = 0; j < pixelCount; j++) {
             if (image[Math.trunc(i / pixelSize)][j]) {
                 ctx.beginPath()
                 ctx.strokeStyle = `rgb(${lineColor[0]},${lineColor[1]},${lineColor[2]})`
@@ -60,6 +68,6 @@ function generateImage(imageSize, imageMapSize, h = 'set', s = 10, v = 30, color
     }
 }
 
-window.addEventListener('load', () => generateImage(imageSize, imageMapSize))
+window.addEventListener('load', () => generateImage())
 button = document.querySelector('#generate')
-button.addEventListener('click', () => generateImage(imageSize, imageMapSize))
+button.addEventListener('click', () => generateImage())
